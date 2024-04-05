de1de6f64019be68388e4a0ae9067f7521d8507f
joselin <a20206442@pucp.edu.pe>
2024-04-05 14:13:59 -0500

    inicio

---
+++ Laboratorios/app/src/main/java/com/example/lab2_20206442/SegundaVista.java
@@ -0,0 +1,26 @@
+package com.example.lab2_20206442;
+
+import android.os.Bundle;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+
+public class SegundaVista extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        EdgeToEdge.enable(this);
+        setContentView(R.layout.segunda_vista);
+
+
+        Toolbar toolbar = findViewById(R.id.toolbar1);
+        setSupportActionBar(toolbar);
+
+    }
+
+}
---
+++ Laboratorios/app/src/main/java/com/example/lab2_20206442/TerceraVista.java
@@ -0,0 +1,4 @@
+package com.example.lab2_20206442;
+
+public class TerceraVista {
+}
